90a9868d2b2eff1654d298a78d72d7b196419f22
chenkang <115397346+chinatownlittlewhite@users.noreply.github.com>
2023-09-06 20:28:18 +0800

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,43 @@
+(function($, document, window){
+	
+	$(document).ready(function(){
+
+		// Cloning main navigation for mobile menu
+		$(".mobile-navigation").append($(".main-navigation .menu").clone());
+
+		// Mobile menu toggle 
+		$(".menu-toggle").click(function(){
+			$(".mobile-navigation").slideToggle();
+		});
+
+		$(".hero").flexslider({
+			directionNav: false,
+			controlNav: true,
+		});
+
+		var map = $(".map");
+		var latitude = map.data("latitude");
+		var longitude = map.data("longitude");
+		if( map.length ){
+			
+			map.gmap3({
+				map:{
+					options:{
+						center: [latitude,longitude],
+						zoom: 15,
+						scrollwheel: false
+					}
+				},
+				marker:{
+					latLng: [latitude,longitude],
+				}
+			});
+			
+		}
+	});
+
+	$(window).load(function(){
+
+	});
+
+})(jQuery, document, window);
